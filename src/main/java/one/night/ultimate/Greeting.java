aeaef206a8f8c6036dcdb73a3eb12d7c5221ad7b
zliu <zliu@>
2017-03-13 19:18:43 +0800

    first

---
+++ src/main/java/one/night/ultimate/Greeting.java
@@ -0,0 +1,18 @@
+package one.night.ultimate;
+
+public class Greeting {
+	private String content;
+
+	public Greeting() {
+
+	}
+
+	public Greeting(String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+}
